36eebcec52116617810046376fa9413d0ee4693d
Priit Lätt <priit@greenhouseci.com>
2017-05-23 12:44:02 +0300

    Initial commit

---
+++ install_versions.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+
+gradle_wrapper_properties_template = """\
+#Mon Apr 24 12:14:06 EEST 2017
+distributionBase=GRADLE_USER_HOME
+distributionPath=wrapper/dists
+zipStoreBase=GRADLE_USER_HOME
+zipStorePath=wrapper/dists
+distributionUrl=https\://services.gradle.org/distributions/gradle-{version}-all.zip
+"""
+
+versions = [
+    '1.6', '1.7', '1.8', '1.9', '1.10', '1.11', '1.12',
+    '2.0', '2.1', '2.2', '2.2.1', '2.3', '2.4', '2.5',
+    '2.6', '2.7', '2.8', '2.9', '2.10', '2.11', '2.12',
+    '2.13', '2.14', '2.14.1', '3.0', '3.1', '3.2',
+    '3.2.1', '3.3', '3.4', '3.4.1', '3.5', '4.0-milestone-1'
+]
+
+for version in versions:
+    gradle_wrapper_properties = gradle_wrapper_properties_template.format(version=version)
+    with open('gradle/wrapper/gradle-wrapper.properties', 'w') as fd:
+        fd.write(gradle_wrapper_properties)
+
+    print("\n\n== Gradle version %s ==\n\n" % version)
+    return_code = subprocess.call(['./gradlew', 'tasks'])
+    if return_code != 0:
+        raise ValueError("Installing Gradle %s failed" % version)
