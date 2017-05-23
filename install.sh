6e005b4dcca3cba4865a5803ab905d57ef96f665
Priit Lätt <priit@greenhouseci.com>
2017-05-23 13:07:00 +0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+set -x
+
+ARCHIVE_PATH="/tmp/install-gradles.zip"
+REPO_DIR="install-gradle-versions-master"
+
+wget https://github.com/priitlatt/install-gradle-versions/archive/master.zip -O $ARCHIVE_PATH
+unzip $ARCHIVE_PATH
+
+CURRENT_DIR=`pwd`
+cd $REPO_DIR
+python install_versions.py
+
+cd $CURRENT_DIR
+rm $ARCHIVE_PATH
+rm -r $REPO_DIR
+
